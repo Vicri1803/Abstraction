ccb1a7215a2f83082e02fea3cf59e094e5a201b2
Muhammad Vicri Ariadi <62140544+Vicri1803@users.noreply.github.com>
2020-07-05 11:55:18 +0700

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Abstraction.AbstractClass;    //Jika menggunakan Abstract Class
+using Abstraction.Interface;        //Jika menggunakan Interface
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama\t: Naima Zulfa");
+            Console.WriteLine("NIM\t: 19.11.2735");
+            Console.WriteLine("Kelas\t: 19 IF 03");
+
+            Console.WriteLine();
+            Console.WriteLine("++++ Negara ++++");
+            Console.WriteLine();
+
+            Negara negara;
+
+            negara = new Indonesia();
+            negara.nmanegara();
+            negara.nmaik();
+            negara.populasi();
+
+            Console.WriteLine();
+            negara = new Rusia();
+            negara.nmanegara();
+            negara.nmaik();
+            negara.populasi();
+
+            Console.WriteLine();
+            negara = new Jepang();
+            negara.nmanegara();
+            negara.nmaik();
+            negara.populasi();
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("++++ Music Genre ++++");
+            Console.WriteLine();
+
+            IGenre genre;
+
+            genre = new Pop();
+            genre.jenis();
+            genre.info();
+
+            Console.WriteLine();
+            genre = new Jazz();
+            genre.jenis();
+            genre.info();
+
+            Console.WriteLine();
+            genre = new Hiphop();
+            genre.jenis();
+            genre.info();
+
+            Console.ReadKey();
+        }
+    }
+}
